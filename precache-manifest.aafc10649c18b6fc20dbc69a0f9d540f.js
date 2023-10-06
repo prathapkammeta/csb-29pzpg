03a5769c75f29034de2245ec32ac298c2dcebdf5
Code Sandbox <hello@codesandbox.io>
2023-10-06 15:36:23 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.aafc10649c18b6fc20dbc69a0f9d540f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63e31353463f9fd4d1160d9c5dec3f9",
+    "url": "https://prathapkammeta.github.io/csb-29pzpg/index.html"
+  },
+  {
+    "revision": "dccf3c7c9e67f1db9683",
+    "url": "https://prathapkammeta.github.io/csb-29pzpg/static/css/main.70d15413.chunk.css"
+  },
+  {
+    "revision": "17c2233e815b4ec271fb",
+    "url": "https://prathapkammeta.github.io/csb-29pzpg/static/js/2.e7a16713.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://prathapkammeta.github.io/csb-29pzpg/static/js/2.e7a16713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccf3c7c9e67f1db9683",
+    "url": "https://prathapkammeta.github.io/csb-29pzpg/static/js/main.0030c96a.chunk.js"
+  },
+  {
+    "revision": "1b0d9907e04e08f467f0",
+    "url": "https://prathapkammeta.github.io/csb-29pzpg/static/js/runtime-main.a440803c.js"
+  }
+]);
